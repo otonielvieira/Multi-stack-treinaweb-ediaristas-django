bd148d890f3b940df47ea26936fe0b98afde85f1
otonielvieira <ottobuzios@gmail.com>
2022-08-06 17:15:13 -0300

    Listagem de usuário concluída

--- administracao/urls.py
+++ administracao/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('servicos/cadastrar/', servico_views.cadastrar_servico, name='cadastrar_servico'),
     path('servicos/listar_servicos', servico_views.listar_servicos, name="listar_servicos"),
     path('servicos/editar/<int:id>', servico_views.editar_servico, name='editar_servico'),
-    path('usarios/cadastrar', usuario_views.cadastrar_usuario, name="cadatrar_usuario")
+    path('usuarios/cadastrar', usuario_views.cadastrar_usuario, name="cadastrar_usuario"),
+    path('usuarios/listar', usuario_views.listar_usuarios, name="listar_usuarios" ),
 
   
 ]
