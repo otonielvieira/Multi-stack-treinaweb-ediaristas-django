224468be8886621c96bbd0e9e4ef1620d15a3283
otonielvieira <ottobuzios@gmail.com>
2022-11-15 06:29:38 -0300

    Criando endpoint para listar todos os servicos

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import endereco_cep_view, diaristas_localidade_view, disponibilidade_atendimento_cidade
+from .views import endereco_cep_view, diaristas_localidade_view, disponibilidade_atendimento_cidade, servico_view
 
 
 urlpatterns = [
      path('diaristas/localidades', diaristas_localidade_view.DiaristasLocalidades.as_view(), name='ediaristas-localidades-list'),
      path('enderecos', endereco_cep_view.EnderecoCep.as_view(), name="endereco-cep-list"),
      path('diaristas/disponibilidade', disponibilidade_atendimento_cidade.DisponibilidadeAtendimentoCidade.as_view(),
-     name='disponibilidade-atendimento-cidade-list')
+     name='disponibilidade-atendimento-cidade-list'),
+     path('servicos', servico_view.Servico.as_view(), name='servico-list')
      
 ]
 
