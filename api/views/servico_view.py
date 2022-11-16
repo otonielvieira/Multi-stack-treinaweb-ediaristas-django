3b788c34bdee2b7b61e51d56ecdf136e650a587a
otonielvieira <ottobuzios@gmail.com>
2022-11-16 05:44:22 -0300

    Adicionando camada de servico buscar servicos

--- api/views/servico_view.py
+++ api/views/servico_view.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status as status_http
-from administracao.models import Servico as ServicoMOdel
+from administracao.services import servico_service
 from ..serializers import servico_serializer
 
 
 class Servico(APIView):
     def get(self, request, format=None):
-        servicos = ServicoMOdel.objects.order_by('posicao')
+        servicos = servico_service.listar_servicos()
         serializer_servico = servico_serializer.ServicoSerializer(servicos, many=True)
         return Response(serializer_servico.data, status=status_http.HTTP_200_OK)
