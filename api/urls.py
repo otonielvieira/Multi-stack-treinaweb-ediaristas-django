df932d0e5c900da2709f1cf70f14f3c49d134b3d
otonielvieira <ottobuzios@gmail.com>
2022-11-17 06:07:50 -0300

    Retornando hateoas

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import endereco_cep_view, diaristas_localidade_view, disponibilidade_atendimento_cidade, servico_view
+from .views import endereco_cep_view, diaristas_localidade_view, disponibilidade_atendimento_cidade, servico_view, inicio_view
 
 
 urlpatterns = [
@@ -7,7 +7,9 @@ urlpatterns = [
      path('enderecos', endereco_cep_view.EnderecoCep.as_view(), name="endereco-cep-list"),
      path('diaristas/disponibilidade', disponibilidade_atendimento_cidade.DisponibilidadeAtendimentoCidade.as_view(),
      name='disponibilidade-atendimento-cidade-list'),
-     path('servicos', servico_view.Servico.as_view(), name='servico-list')
+     path('servicos', servico_view.Servico.as_view(), name='servico-list'),
+     
+     path('', inicio_view.Inicio.as_view(), name='inicio')
      
 ]
 
